a1374913ffbf28898fe2cad06f0e7598f783057d
Karla Abad <kabad1014@gmail.com>
2023-05-03 13:41:45 -0400

    Adding typing to reusable components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
+import { useState } from "react";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
 import ListGroup from "./components/ListGroup";
 
 function App() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
+  const [alertVisible, setAlertVisible] = useState(false);
+  const items = ["New York", "Los Angeles", "San Francisco"];
 
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisible(false)}>My alert</Alert>
+      )}
+      <Button onClick={() => setAlertVisible(true)}>My Button</Button>
+      <ListGroup items={items} heading="Miami" onSelectItem={() => {}} />
     </div>
   );
 }
